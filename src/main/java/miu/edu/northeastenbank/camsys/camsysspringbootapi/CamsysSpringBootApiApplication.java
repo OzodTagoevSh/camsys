2e259a6c03d1fb8539183cce9dcc3430dac5badc
Ozod Tagoev <33785422+OzodTagoevSh@users.noreply.github.com>
2023-03-02 10:23:47 -0600

    First commit

---
+++ src/main/java/miu/edu/northeastenbank/camsys/camsysspringbootapi/CamsysSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.northeastenbank.camsys.camsysspringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamsysSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamsysSpringBootApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miu/edu/northeastenbank/camsys/camsysspringbootapi/CamsysSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.northeastenbank.camsys.camsysspringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamsysSpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
